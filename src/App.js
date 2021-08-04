d74e30ad34ac763393226112b75e23112673c3d0
bongofury <jtroutmanman@gmail.com>
2021-08-04 18:57:13 -0500

    commit tree aaa3ee34f4fe60118b38a39618bb6a54d67ebae9

--- src/App.js
+++ src/App.js
@@ -1,1 +1,34 @@
+import React, { Component } from "react";
+
 // create your App component here
+class App extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            astronauts: []
+        };
+    }
+
+    render() {
+        let names = this.state.astronauts.map(person => person.name);
+        let displayNames = (names) => names.map(name => <li key={name}>{name}</li>)
+        return (
+            <div>
+                <h1>Astronauts In Space</h1>
+                <ol>{displayNames(names)}</ol>
+            </div>
+        )
+    }
+
+    componentDidMount() {
+        fetch('http://api.open-notify.org/astros.json').then(resp => resp.json()).then(data => {
+            this.setState({
+                astronauts: data.people
+            })
+        })
+    }
+}
+
+
+export default App;
